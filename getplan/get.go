1606fb0fdd27c79127de831ccb9bc8a6bc3f1dad
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-11-18 08:02:36 +0100

    So heuristic... But works!

--- getplan/get.go
+++ getplan/get.go
@@ -3,50 +3,60 @@ package getplan
 import (
 	"bytes"
 	"fmt"
-	"io"
+	//	"io"
 	"io/ioutil"
-	"log"
+	//"log"
 	"net/http"
-	"net/url"
+	//	"net/url"
 	"os"
 	"regexp"
-	"strings"
+	//	"strings"
 )
+
+func mergebytesandgivestring(b [][]byte) string {
+	var c bytes.Buffer
+	for _, j := range b {
+		c.Write(j)
+	}
+	return c.String()
+}
 
 func GetInfo() (string, []string) {
 	we, err := http.Get(`http://loiv.torun.pl/index.php/pl/dla-uczniow/organizacja-zajec/plan-lekcji`)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 	}
-	replan := regexp.MustCompile(`attachments.*plan_KLAS.pdf`)
-	rezast := regexp.MustCompile(`attachments.*ast.*pstwa.*pdf`)
+	replan := regexp.MustCompile(`attachments.{6,26}plan_KLAS.pdf`)
+	rezast := regexp.MustCompile(`attachments.{6,26}ast.{0,6}pstwa.{0,15}pdf`)
 	//ourw := make([]byte, 0, 100)
 	ourw, werr := ioutil.ReadAll(we.Body)
 	//wn, werr := we.Body.Read(ourp)
 	if werr != nil {
-		log.Fatal(perr)
+		//log.Fatal(string(werr.Error()))
 	}
 	wplan := replan.FindAll(ourw, -1)
 	wzast := rezast.FindAll(ourw, -1)
-	if len(wplan) > 0 {
-		log.Fatal(wplan)
+	fmt.Println(mergebytesandgivestring(wplan))
+	fmt.Println(mergebytesandgivestring(wzast))
+	if len(wplan) > 1 {
+		//log.Fatal(wplan)
 		fmt.Println("Too long!")
 	}
 	oz := make([]string, 0, 10)
 	for i := range wzast {
-		oz = append(oz, wzast[i])
+		oz = append(oz, string(wzast[i]))
 	}
 	return string(wplan[0]), oz
 }
 
 func GetPlan(u string) []byte {
-	we, err := http.Get(`http://loiv.torun.pl/` + url.QueryEscape(u))
+	we, err := http.Get(`http://loiv.torun.pl/` + u)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(string(err.Error()))
 	}
 	naszplik, err := ioutil.ReadAll(we.Body)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(string(err.Error()))
 	}
 	return naszplik
 }
@@ -62,28 +72,56 @@ func SaveZasts(ul []string) {
 
 //func SavePlan
 
-func FindByte(s string, char byte) int {
+func findByte(s string, char byte) int {
 	bs := []byte(s)
 	last := 0
 	for i := range bs {
 		if bs[i] == char {
 			last = i
+			fmt.Println(string(bs[i+1:]))
 		}
 	}
-	return last
+	return last + 1
 }
 
 func DajNazwePliku(s string) string {
-	last := FindByte(s, byte(`/`))
-	return string([]byte(s)[:last])
+	last := findByte(s, []byte(`/`)[0])
+	a := string([]byte(s)[last:])
+	fmt.Println(a)
+	return a
 }
 
-func ZapiszPlik(cozap []byte, nazwa string) {
-	if _, err := os.Stat(nazwa); err == nil {
-		if bytes.Equal(cozap, ioutil.ReadAll(os.Open(nazwa))) {
+func ZapiszPlik(cozap []byte, url string) {
+	nazwa := DajNazwePliku(url)
+	_, err := os.Stat(`./` + nazwa)
+	if err == nil {
+		fileopen, openerr := os.Open(`./` + nazwa)
+		if openerr != nil {
+			//log.Fatal(string(openerr.Error()))
+		}
+		fileread, readallerr := ioutil.ReadAll(fileopen)
+		if readallerr != nil {
+			//log.Fatal(string(readallerr.Error))
+		}
+		if bytes.Equal(cozap, fileread) {
 			return
 		}
+	} else {
+		os.Create(`./` + nazwa)
 	}
-	ioutil.WriteFile(nazwa, cozap, 0644)
+	fmt.Println(err)
+	err = ioutil.WriteFile(`./`+nazwa, cozap, 0644)
+	fmt.Println(err)
 	return
+}
+
+func FullService() {
+	pi, zi := GetInfo()
+	fmt.Println(1)
+	p := GetPlan(pi)
+	fmt.Println(2)
+	ZapiszPlik(p, pi)
+	fmt.Println(3)
+	SaveZasts(zi)
+	fmt.Println(4)
 }
